157f4394dfe56f51514c6ff85d3c202e0e75fb4a
Bhavleenk <bhavcode@gmail.com>
2023-03-18 11:33:37 +0530

    Functions

--- main.py
+++ main.py
@@ -36,6 +36,102 @@ def draw_word():
             print("_", end=" ")
     print("")
 
+def draw_hangman():
+    global lives_left
+    if lives_left == 6:
+        print("+------------+")
+        print("|            |")
+        print("|")
+        print("|")
+        print("|")
+        print("|")
+        print("|")
+        print("+-------+")
+    elif lives_left == 5:
+        print("+------------+")
+        print("|            |")
+        print("|            O")
+        print("|")
+        print("|")
+        print("|")
+        print("|")
+        print("+-------+")
+    elif lives_left == 4:
+        print("+------------+")
+        print("|            |")
+        print("|            O")
+        print("|            |")
+        print("|")
+        print("|")
+        print("|")
+        print("+-------+")
+    elif lives_left == 3:
+        print("+------------+")
+        print("|            |")
+        print("|            O")
+        print("|            |")
+        print("|           /")
+        print("|")
+        print("|")
+        print("+-------+")
+    elif lives_left == 2:
+        print("+------------+")
+        print("|            |")
+        print("|            O")
+        print("|            |")
+        print("|           / \\")
+        print("|")
+        print("|")
+        print("+-------+")
+    elif lives_left == 1:
+        print("+------------+")
+        print("|            |")
+        print("|            O")
+        print("|            |\\")
+        print("|           / \\")
+        print("|")
+        print("|")
+        print("+-------+")
+    elif lives_left == 0:
+        print("+------------+")
+        print("|            |")
+        print("|            O")
+        print("|           /|\\")
+        print("|           / \\")
+        print("|")
+        print("|")
+        print("+-------+")
+
+def get_one_valid_letter():
+    is_letter_valid = False
+    letter = ""
+    while is_letter_valid is False:
+        letter = input("Enter guess letter: ")
+        letter = letter.strip().lower()
+        if len(letter) <= 0 or len(letter) > 1:
+            print("Letter must be of length 1")
+        elif letter.isalpha():
+            if letter in correctly_guessed_letters or letter in incorrectly_guessed_letters:
+                print("You have already guessed the letter " + letter + ", please try again")
+            else:
+                is_letter_valid = True
+        else:
+            print("Letter must be (a-z)")
+
+    return letter
+
+def guess_letter():
+    global correctly_guessed_letters
+    global incorrectly_guessed_letters
+    global lives_left
+
+    letter = get_one_valid_letter()
+    if letter in randomly_chosen_word:
+        correctly_guessed_letters.append(letter)
+    else:
+        incorrectly_guessed_letters.append(letter)
+        lives_left -= 1
+
 
 
 
